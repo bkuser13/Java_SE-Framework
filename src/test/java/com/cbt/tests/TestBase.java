eb3f9bf1ef02ab831fa43985c9b94e981743802b
Bolot <bkuser13@gmail.com>
2018-07-19 21:42:41 -0500

    First

---
+++ src/test/java/com/cbt/tests/TestBase.java
@@ -0,0 +1,34 @@
+package com.cbt.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.cbt.utilities.ConfigurationReader;
+import com.cbt.utilities.Driver;
+
+public abstract class TestBase {
+	protected WebDriver driver;
+	protected Actions actions;
+	
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() {
+		driver = Driver.getDriver();
+		actions = new Actions(driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+		//driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+		driver.get(ConfigurationReader.getProperty("url"));
+
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() {
+		Driver.closeDriver();
+	}
+
+}
